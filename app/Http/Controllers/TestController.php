b7f25f0ce3ccb0ddff77f9aafb328ba104d1c233
far666 <nrs666666@gmail.com>
2018-06-24 17:49:16 +0800

    practice router

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class TestController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        return "this is TestController's index";
+    }
+
+    public function hello($name)
+    {
+        return sprintf("Hi! %s!", $name);
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,25 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('test/basic_routing', function () {
+    return "this is use the basic routing";
+});
+
+$router->get('test/basic_routing/{parameter}', function ($parameter) {
+    return sprintf("this is use the basic routing with parameter: `%s`", $parameter);
+});
+
+$router->get('test/basic_routing/{parameter}', function ($parameter) {
+    return sprintf("this is use the basic routing with parameter: `%s`", $parameter);
+});
+
+// can use on check user is login or not 
+$router->group(['middleware' => App\Http\Middleware\TestMiddleware::class], function () use ($router) {
+    $router->get('test/group_and_middleware', function ()    {
+        return "group share route attributes, such as middleware";
+    });
+});
+
+$router->get('test/use_contoller',  'TestController@index');
+$router->get('test/use_contoller/hello/{name}',  'TestController@hello');
